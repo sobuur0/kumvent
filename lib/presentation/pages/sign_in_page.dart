cf7fbe961ed759b1981b97fadaa87bf776c27965
t-bolt <isholasobuur@gmail.com>
2022-04-04 13:42:37 +0100

    added page structure

--- lib/presentation/pages/sign_in_page.dart
+++ lib/presentation/pages/sign_in_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
@@ -8,8 +10,54 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  late bool _isChecked = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Text(
+              'Create a new Account',
+            ),
+            const Text(
+              'Name'
+            ),
+            TextFormField(),
+            const Text(
+              'Email Address'
+            ),
+            TextFormField(),
+            const Text(
+              'Password'
+            ),
+            TextFormField(),
+            Row(
+              children: [
+                Checkbox(
+                  value: _isChecked,
+                  onChanged: (value) {
+                    setState(() {
+                      _isChecked = true;
+                    });
+                  },
+                ),
+                const Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(text: 'I agree to'),
+                      TextSpan(text: 'Terms of Service'),
+                      TextSpan(text: 'and'),
+                      TextSpan(text: 'Privacy Policy'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
