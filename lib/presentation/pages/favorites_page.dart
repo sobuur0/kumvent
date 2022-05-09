ed7fa496459b1c8d034ba3daece81e3be6a76b16
John-soft <akinwalereoluwaseun60@gmail.com>
2022-05-09 12:05:46 +0100

    added f

--- lib/presentation/pages/favorites_page.dart
+++ lib/presentation/pages/favorites_page.dart
@@ -20,8 +20,46 @@ class FavoritesPage extends StatelessWidget {
           color: kLeadingIconColor,
         ),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 27),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 96,
+              padding: const EdgeInsets.all(12.0),
+              margin: const EdgeInsets.only(left: 16, right: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade300,
+                      offset: const Offset(0.45, 0.5),
+                      blurRadius: 20,
+                      spreadRadius: 0.15),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Image.asset('images/favorite.png'),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Atlantis Event Center',
+                      ),
+                      Text('Lekki, Logos'),
+                      Text('4.9'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
