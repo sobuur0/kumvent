839373cb4ffea71d8952ef28ef832ecadf8de90e
John-soft <akinwalereoluwaseun60@gmail.com>
2022-05-12 17:44:38 +0100

    checkout page 90% done

--- lib/presentation/widgets/build_card_widget.dart
+++ lib/presentation/widgets/build_card_widget.dart
@@ -21,73 +21,66 @@ class BuildCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0),
-      child: Row(
-        children: [
-          Container(
-            height: 168,
-            width: 330,
-            decoration: BoxDecoration(
-              color: bgColor,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 30,
-                  left: 50,
-                  child: Text(
-                    cardType,
-                    style:
-                        TextStyles.bold(color: kWhiteShadeColor, fontSize: 16),
-                  ),
-                ),
-                Positioned(
-                  left: 50,
-                  top: 79,
-                  child: Text(
-                    cardNumber,
-                    style:
-                        TextStyles.bold(color: kWhiteShadeColor, fontSize: 14),
-                  ),
-                ),
-                Positioned(
-                  top: 132,
-                  left: 50,
-                  child: Text(
-                    cardHolderName,
-                    style:
-                        TextStyles.bold(color: kGreyShadeColor, fontSize: 12),
-                  ),
-                ),
-                const Positioned(
-                  top: 15,
-                  right: 60,
-                  child: Image(
-                    image: AssetImage(
-                      'images/mastercard-logo.png',
-                    ),
-                    height: 48,
-                    width: 48,
-                  ),
-                ),
-                Positioned(
-                  bottom: 21,
-                  right: 60,
-                  child: Text(
-                    expDate,
-                    style:
-                        TextStyles.bold(color: kGreyShadeColor, fontSize: 12),
-                  ),
-                ),
-              ],
+    return Row(
+      children: [
+        Container(
+          height: 168,
+          width: 330,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(10),
             ),
           ),
-        ],
-      ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 30,
+                left: 50,
+                child: Text(
+                  cardType,
+                  style: TextStyles.bold(color: kWhiteShadeColor, fontSize: 16),
+                ),
+              ),
+              Positioned(
+                left: 50,
+                top: 79,
+                child: Text(
+                  cardNumber,
+                  style: TextStyles.bold(color: kWhiteShadeColor, fontSize: 14),
+                ),
+              ),
+              Positioned(
+                top: 132,
+                left: 50,
+                child: Text(
+                  cardHolderName,
+                  style: TextStyles.bold(color: kGreyShadeColor, fontSize: 12),
+                ),
+              ),
+              const Positioned(
+                top: 15,
+                right: 60,
+                child: Image(
+                  image: AssetImage(
+                    'images/mastercard-logo.png',
+                  ),
+                  height: 48,
+                  width: 48,
+                ),
+              ),
+              Positioned(
+                bottom: 21,
+                right: 60,
+                child: Text(
+                  expDate,
+                  style: TextStyles.bold(color: kGreyShadeColor, fontSize: 12),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
