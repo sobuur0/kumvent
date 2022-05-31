726be69ff297113b8d90a3ecd401fe7049628955
John-soft <akinwalereoluwaseun60@gmail.com>
2022-05-31 14:09:47 +0100

    updates the favorites screen

--- lib/presentation/pages/favorites_page.dart
+++ lib/presentation/pages/favorites_page.dart
@@ -6,7 +6,7 @@ import 'package:kumvent/presentation/widgets/favorite_widget.dart';
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({Key? key}) : super(key: key);
   static String routeName = '/fav_page';
-
+  //updating the favorites page
   final String image = 'images/atlantis_center.png';
 
   @override
