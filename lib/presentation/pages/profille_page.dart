154896978ccb66ec312db663ca5491a458fd31a4
t-bolt <isholasobuur@gmail.com>
2022-06-08 17:26:47 +0100

    okay!!! next feature?

--- lib/presentation/pages/profille_page.dart
+++ lib/presentation/pages/profille_page.dart
@@ -26,8 +26,11 @@ class _ProfilePageState extends State<ProfilePage> {
         .collection("users")
         .doc(user!.uid)
         .get()
-        .then((value) => currentUser = UserModel.fromMap(value.data()));
-    setState(() {});
+        .then((value) {
+      setState(() {});
+      currentUser = UserModel.fromMap(value.data());
+    });
+
     super.initState();
   }
 
@@ -36,94 +39,78 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        leading: InkWell(
-          onTap: () => Navigator.pop(context),
-          child: const Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-        ),
       ),
       body: SafeArea(
-        child: ListView(
+        child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          children: [
-            Center(
-              child: Image.asset(
-                'images/profile_image.png',
-                height: 80,
-                width: 80,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                '${currentUser.fullName}',
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xff061A60),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  'images/profile_image.png',
+                  height: 80,
+                  width: 80,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 56.0,
-            ),
-            ProfileCard(
-              leadingIcon: Icons.person,
-              title: 'Edit profile',
-              onPressed: () {},
-            ),
-            ProfileCard(
-              leadingIcon: Icons.history,
-              title: 'History',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const FavoritesPage(),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Text(
+                  '${currentUser.fullName}',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xff061A60),
                   ),
-                );
-              },
-            ),
-            ProfileCard(
-              leadingIcon: Icons.feedback_outlined,
-              title: 'About App',
-              onPressed: () {},
-            ),
-            ProfileCard(
-              leadingIcon: Icons.payment_outlined,
-              title: 'Payment',
-              onPressed: () {},
-            ),
-            ProfileCard(
-              leadingIcon: Icons.help,
-              title: 'Help Center',
-              onPressed: () {},
-            ),
-            ProfileCard(
-              leadingIcon: Icons.logout,
-              title: 'Logout',
-              onPressed: () {
-                _logoutUser();
-                Fluttertoast.showToast(
-                    msg: 'You have successfully been logged out');
-              },
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 8.0)),
-            const Center(
-              child: Text(
-                'Version 0.4.465',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w400,
                 ),
               ),
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 20.0)),
-          ],
+              const Spacer(),
+              ProfileCard(
+                leadingIcon: Icons.person,
+                title: 'Edit profile',
+                onPressed: () {},
+              ),
+              ProfileCard(
+                leadingIcon: Icons.history,
+                title: 'History',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const FavoritesPage(),
+                    ),
+                  );
+                },
+              ),
+              ProfileCard(
+                leadingIcon: Icons.payment_outlined,
+                title: 'Payment',
+                onPressed: () {},
+              ),
+              ProfileCard(
+                leadingIcon: Icons.logout,
+                title: 'Logout',
+                onPressed: () {
+                  _logoutUser();
+                  Fluttertoast.showToast(
+                      msg: 'You have successfully been logged out');
+                },
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 8.0)),
+              const Center(
+                child: Text(
+                  'Version 0.4.465',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 20.0)),
+            ],
+          ),
         ),
       ),
     );
